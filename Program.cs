47ec4e607ff89af1d5c6cdd7e5d590fb4c100ad0
rustleupchef <rustle.up.chef@gmail.com>
2025-02-07 20:26:45 -0600

    Added task part in source.json

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Sockets;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
 using Newtonsoft.Json;
 
 namespace Ring_Zero
@@ -13,12 +14,15 @@ namespace Ring_Zero
             string data = File.ReadAllText("source.json");
             dynamic json = JsonConvert.DeserializeObject(data);
             int source = (int) json["source"];
+            
             bool isOllama = (bool) json["isOllamaModel"];
-
+            string task = (string) json["task"];
+            
+            
             // use camera if source isn't -1
             if (source >= 0)
             {
-                VideoCapture capture = new VideoCapture(source);
+                VideoCapture capture = new(source);
                 capture.Start();
                 while (true)
                 {
@@ -36,7 +40,7 @@ namespace Ring_Zero
                 }
             }
             
-            using (TcpListener socket = new TcpListener(IPAddress.Any, 8080))
+            using (TcpListener socket = new(IPAddress.Any, 8080))
             {
                 socket.Start();
 
@@ -44,23 +48,22 @@ namespace Ring_Zero
                 {
                     TcpClient client = socket.AcceptTcpClient();
                     NetworkStream stream = client.GetStream();
-                    BinaryReader br = new BinaryReader(stream);
+                    BinaryReader br = new(stream);
                     
                     // grabbing length of buffer
                     byte[] lengthBuffer = new byte[4];
                     br.Read(lengthBuffer, 0, 4);
-                    if (BitConverter.IsLittleEndian)
-                    {
-                        lengthBuffer = lengthBuffer.Reverse().ToArray();
-                    }
+                    if (BitConverter.IsLittleEndian) lengthBuffer = lengthBuffer.Reverse().ToArray();
                     int length = BitConverter.ToInt32(lengthBuffer, 0);
                     
                     // reading buffer fully
                     byte[] buffer = new byte[length];
-                    for (int bytesRead = 0; bytesRead < length; bytesRead += stream.Read(buffer, bytesRead, length - bytesRead));
+                    for (int bytesRead = 0;
+                         bytesRead < length; 
+                         bytesRead += stream.Read(buffer, bytesRead, length - bytesRead));
                     
                     // converting buffer to frame
-                    Mat frame = new Mat();
+                    Mat frame = new();
                     CvInvoke.Imdecode(buffer, ImreadModes.Color, frame);
                     CvInvoke.Imshow("frame", frame);
 
@@ -78,6 +81,19 @@ namespace Ring_Zero
                     client.Close();
                 }
             }
+        }
+
+        private static void process(Mat frame, bool isOllama)
+        {
+            // TODO: handle situation where not using ollama
+            if (!isOllama)
+            {
+                return;
+            }
+
+            VectorOfByte buffer = new();
+            CvInvoke.Imencode(".jpg", frame, buffer);
+            byte[] jpeg = buffer.ToArray();
         }
     }
 }
